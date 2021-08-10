f8dbda0b064fe0b5d24522760ef5f0e910679d88
luanbatista904 <luabatista904@outlook.com>
2021-08-10 20:01:22 -0300

    third

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,50 @@
+import 'package:flutter/material.dart';
 
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return () {
+      print('Pergunta Respondida! #02');
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
